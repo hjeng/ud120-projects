80ae02b0f47e6b64baa172364ebe1ccd45b48a93
Danny Jeng <jeng.danny@gmail.com>
2018-12-21 16:16:07 -0500

    Add time taken for training and prediction

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -29,8 +29,14 @@ features_train, features_test, labels_train, labels_test = preprocess()
 import numpy as np
 from sklearn.naive_bayes import GaussianNB
 clf = GaussianNB()
+
+t0 = time()
 clf.fit(features_train, labels_train)
+print "Training time:", round(time()-t0,3), "s"
+
+t1 = time()
 pred = clf.predict(features_test)
+print "Prediction time:", round(time()-t1,3), "s"
 
 from sklearn.metrics import accuracy_score
 print accuracy_score(pred, labels_test)
