55183ad5daa8920453d1e968005ff0445de88032
Danny Jeng <jeng.danny@gmail.com>
2018-12-22 00:56:19 -0500

    Change back to full training set

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -28,8 +28,8 @@ features_train, features_test, labels_train, labels_test = preprocess()
 from sklearn.svm import SVC
 clf = SVC(kernel = "rbf", C = 10000) #C = 10000 gives better accuracy than 10, 100, 1000, and 100 000
 
-features_train = features_train[:len(features_train)/100]
-labels_train = labels_train[:len(labels_train)/100]
+#features_train = features_train[:len(features_train)/100]
+#labels_train = labels_train[:len(labels_train)/100]
 
 t0 = time()
 clf.fit(features_train, labels_train)
