2e0c3fc29e649eee65ce5539eb07a5e18cbd83c5
Danny Jeng <jeng.danny@gmail.com>
2018-12-22 14:18:55 -0500

    Predict single element in the list

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -28,19 +28,21 @@ features_train, features_test, labels_train, labels_test = preprocess()
 from sklearn.svm import SVC
 clf = SVC(kernel = "rbf", C = 10000) #C = 10000 gives better accuracy than 10, 100, 1000, and 100 000
 
-#features_train = features_train[:len(features_train)/100]
-#labels_train = labels_train[:len(labels_train)/100]
+features_train = features_train[:len(features_train)/100]
+labels_train = labels_train[:len(labels_train)/100]
 
 t0 = time()
 clf.fit(features_train, labels_train)
 print "Training time", round(time()-t0, 3), "s"
 
 t1 = time()
-pred = clf.predict(features_test)
+pred = clf.predict(features_test[[50]])
 print "Prediction time", round(time()-t1, 3), "s"
 
-from sklearn.metrics import accuracy_score
-print accuracy_score(pred, labels_test)
+print pred
+
+#from sklearn.metrics import accuracy_score
+#print accuracy_score(pred, labels_test)
 #########################################################
 
 
