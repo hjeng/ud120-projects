5ad686ddd9004db4c63fbffb9a6e44c4e91f5cf7
Danny Jeng <jeng.danny@gmail.com>
2018-12-22 00:54:56 -0500

    Add C parameter

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -26,7 +26,7 @@ features_train, features_test, labels_train, labels_test = preprocess()
 ### your code goes here ###
 
 from sklearn.svm import SVC
-clf = SVC(kernel = "rbf")
+clf = SVC(kernel = "rbf", C = 10000) #C = 10000 gives better accuracy than 10, 100, 1000, and 100 000
 
 features_train = features_train[:len(features_train)/100]
 labels_train = labels_train[:len(labels_train)/100]
