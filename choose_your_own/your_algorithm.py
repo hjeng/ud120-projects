36bad882647562170497698e6f0fff88cf0629b8
Danny Jeng <jeng.danny@gmail.com>
2018-12-26 23:43:40 -0500

    Use AdaBoost

--- choose_your_own/your_algorithm.py
+++ choose_your_own/your_algorithm.py
@@ -31,8 +31,8 @@ plt.show()
 ### your code here!  name your classifier object clf if you want the 
 ### visualization code (prettyPicture) to show you the decision boundary
 
-from sklearn import neighbors
-clf = neighbors.KNeighborsClassifier(n_neighbors = 10) #try num of neighbors as 5, 10, 15, 30; 10 seems to work best
+from sklearn.ensemble import AdaBoostClassifier
+clf = AdaBoostClassifier(n_estimators = 100)
 clf.fit(features_train, labels_train)
 pred = clf.predict(features_test)
 
