cc4dcef92bdaca0773c07f1f331f7d39c54b3dfd
Danny Jeng <jeng.danny@gmail.com>
2018-12-26 23:48:36 -0500

    Use Random Forest

--- choose_your_own/your_algorithm.py
+++ choose_your_own/your_algorithm.py
@@ -31,8 +31,8 @@ plt.show()
 ### your code here!  name your classifier object clf if you want the 
 ### visualization code (prettyPicture) to show you the decision boundary
 
-from sklearn.ensemble import AdaBoostClassifier
-clf = AdaBoostClassifier(n_estimators = 100)
+from sklearn.ensemble import RandomForestClassifier
+clf = RandomForestClassifier(n_estimators = 100)
 clf.fit(features_train, labels_train)
 pred = clf.predict(features_test)
 
